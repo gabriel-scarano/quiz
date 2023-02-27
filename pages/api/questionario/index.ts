93c852957e429555abdce22337c58a663a3c3420
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2023-02-27 08:13:35 -0300

    Embaralhar questões

--- pages/api/questionario/index.ts
+++ pages/api/questionario/index.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { embaralhar } from "../../../functions/array";
 import questoes from '../bancoDeQuestoes';
 
 export default function Questionario(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json(questoes.map(questao => questao.id));
+    const ids = questoes.map(questao => questao.id);
+    res.status(200).json(embaralhar(ids));
 }
--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -6,7 +6,7 @@ export default function Questoes(req: NextApiRequest, res: NextApiResponse) {
   const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
 
   if (unicaQuestaoOuNada.length === 1) {
-    const questaoSelecionada = unicaQuestaoOuNada[0];
+    const questaoSelecionada = unicaQuestaoOuNada[0].embaralharRespostas();
     res.status(200).json(questaoSelecionada.paraObjeto());
   } else {
     res.status(204).send({})
