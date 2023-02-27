de0783918589a7bce2b85315546f086a8935edcb
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2023-02-27 07:49:38 -0300

    Convertendo para objeto

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -2,5 +2,5 @@ import { NextApiRequest, NextApiResponse } from "next";
 import questoes from '../bancoDeQuestoes';
 
 export default function Questoes(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(questoes[0]);
+  res.status(200).json(questoes[0].paraObjeto());
 };
