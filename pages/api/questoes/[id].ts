18947714333670b6e2f15600fd75d465a1d079be
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2023-02-27 07:59:31 -0300

    Construindo a API

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -2,5 +2,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 import questoes from '../bancoDeQuestoes';
 
 export default function Questoes(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(questoes[0].paraObjeto());
+  const idSelecionado: number = +req.query.id!;
+  const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+
+  if (unicaQuestaoOuNada.length === 1) {
+    const questaoSelecionada = unicaQuestaoOuNada[0];
+    res.status(200).json(questaoSelecionada.paraObjeto());
+  } else {
+    res.status(204).send({})
+  }
 };
